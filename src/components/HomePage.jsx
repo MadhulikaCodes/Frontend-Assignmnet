30c3f24c2a2a5654f0c91a18ac08b7cf91f3394e
madhulikaa223344 <madhulikaa223344@gmail.com>
2024-07-27 10:51:25 +0530

    final commit 2

--- src/components/HomePage.jsx
+++ src/components/HomePage.jsx
@@ -1,17 +1,31 @@
 import React from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faArrowRight } from "@fortawesome/free-solid-svg-icons";
-import backgroundImage from "./bg1.jpg"; // Adjust the path accordingly
+import backgroundImage from "./images/bg1.jpg";
+import bg7 from "./images/bg7.png";
+import bg3 from "./images/bg3.jpg";
+import bg4 from "./images/bg4.jpg";
+import bg5 from "./images/bg5.jpg";
+import bg6 from "./images/bg6.jpg";
+ // Adjust the path accordingly
 
 const HomePage = () => {
   return (
     <div
-      className="min-h-screen bg-cover bg-center flex flex-col items-center justify-center text-white"
+      className="min-h-screen bg-cover bg-center flex flex-col items-center justify-center text-white py-20"
       style={{ backgroundImage: `url(${backgroundImage})` }}
     >
       <div className="text-center mb-10 flex flex-col">
-        <h1 className="text-4xl font-bold">EMPOWERING THE DIGITAL ERA:</h1>
-        <h2 className="text-3xl mt-2">DECENTRALIZING ENTERTAINMENT</h2>
+        <h1 className="text-4xl font-bold">
+          <span className="text-purple-200">EMPOWERING</span>
+          <span className="text-purple-300"> THE</span>
+          <span className="text-purple-400"> DIGITAL</span>{" "}
+          <span className="text-purple-500">ERA:</span>
+        </h1>
+        <h2 className="text-3xl mt-2">
+          <span className="text-purple-300">DECENTRALIZING </span>
+          <span className="text-purple-500">ENTERTAINMENT</span>
+        </h2>
         <p className="mt-4">
           Join the world's first DePIN: Revolutionizing Content Streaming with
           faster, safer, and cheaper access
@@ -23,6 +37,43 @@ const HomePage = () => {
             className="ml-2 bg-white rounded-full text-purple-600 p-1 "
           />
         </button>
+        <div className=" flex flex-row justify-between gap-20 items-center">
+          <div className="rotate-[-9deg]  ">
+            <img
+              src={bg3}
+              style={{ height: "100px", width: "200px" }}
+              className="rounded-xl shadow-2xl"
+            />
+          </div>
+          <div className="rotate-[8deg]  ">
+            <img
+              src={bg5}
+              style={{ height: "70px", width: "70px" }}
+              className="rounded-xl shadow-2xl"
+            />
+          </div>
+          <div className="rotate-[40deg] my-20">
+            <img
+              src={bg4}
+              style={{ height: "100px", width: "100px" }}
+              className="rounded-xl shadow-2xl"
+            />
+          </div>
+          <div className="rotate-[-5deg]  ">
+            <img
+              src={bg6}
+              style={{ height: "160px", width: "200px" }}
+              className="rounded-xl shadow-2xl"
+            />
+          </div>
+          <div className="rotate-[45deg]  ">
+            <img
+              src={bg7}
+              style={{ height: "120px", width: "200px" }}
+              className="rounded-xl shadow-2xl"
+            />
+          </div>
+        </div>
       </div>
       <div className="grid grid-cols-2 gap-8 font-black">
         <div className="bg-white bg-opacity-80 text-black p-4 rounded-lg text-left">
--- src/components/HomePage2.jsx
+++ src/components/HomePage2.jsx
@@ -2,9 +2,7 @@ import React from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faArrowRight } from "@fortawesome/free-solid-svg-icons";
 
-
-import globeImage from "./bg2.png"; // Replace with actual path
-
+import globeImage from "./images/bg2.png"; // Replace with actual path
 
 const HomePage2 = () => {
   return (
@@ -35,36 +33,41 @@ const HomePage2 = () => {
       </div>
 
       <div className="grid grid-cols-3 gap-8 max-w-4xl mx-auto mb-10">
-        <div className="bg-blue-400 bg-opacity-80 p-4  rounded-lg text-center">
+        <div className="bg-blue-400 bg-opacity-80 p-4  rounded-lg text-left">
           <h3 className="text-2xl font-semibold text-white">Content Nodes</h3>
           <p className="mt-4 mb-10 text-white">
             Content Nodes, Earn rewards for supplying their GPU, Storage and
             bandwidth for dCDN.
           </p>
-          <button className="mt-4 bg-blue-400 hover:bg-blue-500 text-white font-bold py-2 px-4 rounded-full">
+          <button className="mx-10 px-10 bg-white text-black font-bold py-2 rounded-full">
             Deploy
+            <FontAwesomeIcon
+              icon={faArrowRight}
+              className="bg-black text-white rounded-full mx-2"
+            />
           </button>
         </div>
-        <div className="bg-pink-400 bg-opacity-90 p-4 rounded-lg text-center">
+        <div className="bg-pink-400 bg-opacity-90 pl-6 py-2 pb-2 rounded-lg text-left">
           <h3 className="text-2xl font-semibold text-black">Orchestrators</h3>
           <p className="mt-2 text-black">
             Orchestrators will be responsible for connecting with the Content
             Nodes for seamless streaming. Traffic distribution will be based on
             multiple factors like job type, performance.
           </p>
-          <button className="mt-4 bg-pink-400 hover:bg-pink-500 text-black font-bold py-2 px-4 mx-10 rounded-full flex items-center">
-            Coming Soon <FontAwesomeIcon icon={faArrowRight} className="ml-2" />
+          <button className="bg-pink-400 hover:bg-pink-500 text-black font-bold py-1 px-3 rounded-full border-4 border-pink-300 shadow-xl lg:ml-[120px] mb-5 rotate-[-20deg]">
+            Coming Soon
           </button>
         </div>
-        <div className="bg-green-400 bg-opacity-90 p-4 rounded-lg text-center">
+
+        <div className="bg-green-400 bg-opacity-90 pl-6 py-2 rounded-lg text-left">
           <h3 className="text-2xl font-semibold text-white">Validators</h3>
-          <p className="mt-2 mb-10 text-white">
+          <p className="my-2 mb-5 text-white">
             Validators will be responsible for validating the execution of dCDN
             Network. Validators earn rewards from Staking and earn a cut on the
             executions validated.
           </p>
-          <button className="mt-4 bg-green-400 hover:bg-green-500 text-white font-bold py-2 px-4 mx-8 rounded-full flex items-center">
-            Coming Soon <FontAwesomeIcon icon={faArrowRight} className="ml-2" />
+          <button className=" bg-green-400 hover:bg-green-500 text-white font-bold py-1 px-3 rounded-full flex items-center  border-4 border-green-300 shadow-xl lg:ml-[115px] my-2 rotate-[-20deg]">
+            Coming Soon
           </button>
         </div>
       </div>
